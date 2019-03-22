2b3cadad6653efb87d586a2acac9d8098ac4f52f
donoghuc <cas.donoghue@gmail.com>
2019-03-22 11:50:21 -0700

    (TMP) DO NOT MERGE
    This commit is intended to share what I have tried to do to build bolt on el-8. TODO: Ensure this is sane and remove change to puppet-runtime.json

--- configs/platforms/el-8-x86_64.rb
+++ configs/platforms/el-8-x86_64.rb
@@ -6,8 +6,7 @@ platform "el-8-x86_64" do |plat|
 
   plat.provision_with "rpm --import http://yum.puppetlabs.com/RPM-GPG-KEY-puppetlabs"
   plat.add_build_repository "http://enterprise.delivery.puppetlabs.net/#{peversion}/repos/#{plat.get_name}/#{plat.get_name}.repo"
-  plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/yum/pl-build-tools-release-#{plat.get_os_name}-#{plat.get_os_version}.noarch.rpm"
-  plat.provision_with "yum install --assumeyes autoconf automake rsync gcc createrepo make rpmdevtools rpm-libs yum-utils rpm-sign libtool autogen git"
+  plat.provision_with "yum install --assumeyes autoconf automake rsync gcc createrepo make rpmdevtools rpm-libs yum-utils rpm-sign libtool git"
   plat.install_build_dependencies_with "yum install --assumeyes"
-  plat.vmpooler_template "centos-8-x86_64"
+  plat.vmpooler_template "redhat-8-x86_64"
 end
